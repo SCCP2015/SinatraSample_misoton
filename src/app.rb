31213490c13a141dd97e5e1b7c55efa3b9751be9
misoton665 <misoton998@gmail.com>
2015-06-04 06:54:58 +0000

    Delete comment and lengthy codes.

--- src/app.rb
+++ src/app.rb
@@ -20,7 +20,6 @@ class MainApp < Sinatra::Base
      word.id.to_s + ": #{word.msg}" 
     end
     "[id: msg]\n" + words.join(', ') + "\n"
-    # テーブルwordsの全件を取得(データの見せ方は自由)。
   end
 
   get '/words/:id' do
@@ -30,37 +29,29 @@ class MainApp < Sinatra::Base
     else
       "#{word.id} is not found.\n"
     end
-    # パラメータidに対応するwordsテーブルのレコード一件を取得。(idとmsgを表示)
-    # もし対応するidが無ければエラーメッセージを表示。
   end
   
   post '/words' do
     word = Word.create(msg: request.body.gets)
     word.id.to_s
-    # bodyの文字列をmsgとして、新しいwordレコードを一件追加。
-    # レスポンスとして追加時のレコードのidを返す。
   end
   
   put '/words/:id' do
     word = Word.get(params[:id])
-    if word.nil? then
+    if word then
       "false"
     else
       word.update(msg: request.body.gets)
       "true"
     end
-    # パラメータidに対応するwordsテーブルのレコード一件を更新。
-    # 成功した場合、"true" 失敗した場合、"false"をレスポンスとして返す。
   end
 
   delete '/words/:id' do
     word = Word.get(params[:id])
-    if word.nil? then
+    if word then
       "false" 
     else
       word.destroy.to_s
     end
-    # パラメータidに対応するwordsテーブルのレコード一件を削除。
-    # 成功した場合、"true" 失敗した場合、"false"をレスポンスとして返す。
   end
 end
